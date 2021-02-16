cf2868e967932d0cc890ed94d7fa14090823efee
esra akat <383196@ogr.ktu.edu.tr>
2021-02-16 22:18:36 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<nT, aT>
+    {
+        nT[] names;
+        aT[] ages;
+
+        public MyDictionary() //ctor
+        {
+            names = new nT[0];
+            ages = new aT[0];
+        }
+
+        public void Add(nT name, aT age)
+        {
+            nT[] tempName = names;
+
+            for (int i = 0; i < tempName.Length; i++)
+            {
+                names[i] = tempName[i];
+            }
+            names = new nT[names.Length + 1];
+            names[names.Length - 1] = name;
+
+            aT[] tempAge = ages;
+
+            for (int j = 0; j < tempAge.Length; j++)
+            {
+                ages[j] = tempAge[j];
+            }
+            ages = new aT[ages.Length + 1];
+            ages[ages.Length - 1] = age;
+
+            Console.WriteLine(name + ": " + age);
+
+        }
+
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, int > myDictionary= new MyDictionary<string, int>();
+            myDictionary.Add("Esra", 20);
+            myDictionary.Add("Elif", 11);
+            myDictionary.Add("Hasan", 28);
+            
+        }
+    }
+}
